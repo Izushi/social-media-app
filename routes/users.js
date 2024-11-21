c47ebda6d8510176fdc8ca0f0e9ae08dec05312a
Daz <shoeshinesamurai@gmail.com>
2024-11-20 23:16:11 -0800

    Add user information update functionality and organize user registration logic.

--- routes/users.js
+++ routes/users.js
@@ -16,4 +16,17 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User is deleted.");
+    } catch(err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+})
+
 module.exports = router;
