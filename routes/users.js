2baeda7e382b5f43c53b2089d536ccfd0d959e49
Daz <shoeshinesamurai@gmail.com>
2024-11-20 22:16:02 -0800

    Add user information update functionality and organize user registration logic.

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
-// router.get("/", (req, res) => {
-//   res.send("users router");
-// });
-
-// User registration
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = await new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    const user = await newUser.save();
-    return res.status(200).json(user);
-    
-  } catch (err) {
-    return res.status(500).json(err);
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("User info is updated.");
+    } catch(err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
   }
 })
 
